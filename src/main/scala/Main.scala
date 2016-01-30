23e0b084a41bccbc948737053fbc0a50b6629bad
Don Mennerich <don.mennerich@gmail.com>
2016-01-30 13:46:39 -0500

    adding file type and size

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,28 +8,74 @@ import collection.JavaConversions._
 import com.sun.xml.internal.stream.util.ReadOnlyIterator
 
 object Main extends App {
+	
+	case class FileObject(uuid: UUID, size: Long, fileType: String)
+
   val file = new File("test.xml")
   val factory = XMLInputFactory.newInstance()
   val eventReader = factory.createXMLEventReader(new FileReader(file))
-  var uuids = Vector.empty[UUID]
+  var fileObjects = Vector.empty[FileObject]
   
-  while(eventReader.hasNext){ getObjectIdentifier() }
+  while(eventReader.hasNext){ getObject() }
 
-  println(uuids.size)
-  
-  def getObjectIdentifier() {
+  fileObjects.foreach { fileObject => println(fileObject) }
+
+  def getObject() {
   	val event = eventReader.nextEvent()
 	
-	event.getEventType() match {
-	  case XMLStreamConstants.START_ELEMENT => {
-	  	val startElement = event.asStartElement()
-	  	val qName = startElement.getName()
-	  	if(qName.getLocalPart.equals("objectIdentifierValue")){
-	  		var uuid = UUID.fromString(eventReader.nextEvent().toString)
-	  		uuids = uuids ++ Vector(uuid)
-	  	}
-	  }
-	  case _ => 
-	}
+		event.getEventType() match {
+		  case XMLStreamConstants.START_ELEMENT => {
+		  	val startElement = event.asStartElement()
+		  	val qName = startElement.getName()
+		  	if(qName.getLocalPart.equals("objectIdentifierValue")){
+		  		val uuid = UUID.fromString(eventReader.nextEvent().toString)
+		  		//val size = getSize()
+		  		//val fileType = getType()
+		  		fileObjects = fileObjects ++ Vector(new FileObject(uuid, getSize, getType))		  		
+		  	}
+		  }
+		  case _ => 
+		}
   }
+
+  def getType(): String = {
+  	
+  	val event = eventReader.nextEvent()
+		
+		event.getEventType() match {
+  		case XMLStreamConstants.START_ELEMENT => {
+  			val startElement = event.asStartElement()
+  			val qName = startElement.getName()
+  			qName.getLocalPart.equals("formatName") match {
+  				case true => {
+  				  eventReader.nextEvent().toString
+  				}
+  				case false => getType
+  			}
+  		}
+  		case _ => getType
+  	}
+
+  }
+
+  def getSize(): Long = {
+  	
+  	val event = eventReader.nextEvent()
+		
+		event.getEventType() match {
+  		case XMLStreamConstants.START_ELEMENT => {
+  			val startElement = event.asStartElement()
+  			val qName = startElement.getName()
+  			qName.getLocalPart.equals("size") match {
+  				case true => {
+  				  eventReader.nextEvent().toString.toLong
+  				}
+  				case false => getSize
+  			}
+  		}
+  		case _ => getSize
+  	}
+  	
+  }
+
 }
